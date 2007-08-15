ba78c6ef8d6e92be5809f9a4655539ec6558d755
Andrew Bettison <andrewb@zip.com.au>
2007-08-15 15:41:12 +0000

    In six.uniq:  - add uniqa_group() function  - add doctests for existing functions

--- six/uniq.py
+++ six/uniq.py
@@ -3,7 +3,7 @@
 r'''The uniq() function and associated tools.
 '''
 
-__all__ = ['uniq', 'uniq_generator']
+__all__ = ['uniq', 'uniq_generator', 'uniqa_group']
 
 def uniq(seq, key=None):
     r'''Iterate over all elements in a given iterable, omitting elements that
@@ -11,6 +11,16 @@ def uniq(seq, key=None):
     'key' argument is given, it must be a callable that converts each element
     of the sequence into a value that is used for equality comparison instead
     of the element itself.
+
+        >>> list(uniq([1, 2, 3]))
+        [1, 2, 3]
+
+        >>> list(uniq([1, 1, 2, 3]))
+        [1, 2, 3]
+
+        >>> list(uniq([1, 2, 3, 1]))
+        [1, 2, 3]
+
     '''
     u = set()
     for v in seq:
@@ -25,6 +35,16 @@ def uniq(seq, key=None):
 def uniq_generator(func, key=None):
     r'''Decorator for generator functions to ensure that they only return
     unique values.
+
+        >>> @uniq_generator
+        ... def f():
+        ...     yield 1
+        ...     yield 2
+        ...     yield 3
+        ...     yield 1
+        >>> list(f())
+        [1, 2, 3]
+
     '''
     def newfunc(*args, **kwargs):
         return uniq(func(*args, **kwargs), key=key)
@@ -32,3 +52,37 @@ def uniq_generator(func, key=None):
     newfunc.__doc__ = func.__doc__
     newfunc.__module__ = func.__module__
     return newfunc
+
+def uniqa_group(seq, key=None):
+    r'''Iterate over tuples of all elements in a given iterable, combining
+    adjacent elements that compare equal into a single tuple.  If the 'key'
+    argument is given, it must be a callable that converts each element of the
+    sequence into a value that is used for equality comparison instead of the
+    element itself.
+
+        >>> list(uniqa_group([1, 2, 3]))
+        [(1,), (2,), (3,)]
+
+        >>> list(uniqa_group([1, 1, 2, 3]))
+        [(1, 1), (2,), (3,)]
+
+        >>> list(uniqa_group([1, 2, 3, 1]))
+        [(1,), (2,), (3,), (1,)]
+
+        >>> list(uniqa_group([1, 2, 3, 3]))
+        [(1,), (2,), (3, 3)]
+
+    '''
+    values = []
+    for v in seq:
+        if key:
+            k = key(v)
+        else:
+            k = v
+        if values and lastkey != k:
+            yield tuple(values)
+            values = []
+        values.append(v)
+        lastkey = k
+    if values:
+        yield tuple(values)
